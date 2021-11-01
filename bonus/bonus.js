6314dcef085b16298542b099dd389f9d335d8d8f
alfredochs <chanta.alfredo@gmail.com>
2021-11-01 15:31:15 +0100

    bonus done

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,51 +1,55 @@
-
 Vue.config.devtools = true;
 new Vue({
     el: "#root",
     data: {
         imgList: [
             {
-                url: 'img/01.JPG',
+                url: 'imgBonus/01.JPG',
                 titolo: "Svezia",
                 paragrafo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
             },
             {
-                url: 'img/02.JPG',
+                url: 'imgBonus/02.JPG',
                 titolo: "Svizzera",
                 paragrafo: 'Lorem ipsum',
             },
             {
-                url: 'img/03.JPG',
+                url: 'imgBonus/03.JPG',
                 titolo: "Gran Bretagna",
                 paragrafo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
             },
             {
-                url: 'img/04.JPG',
+                url: 'imgBonus/04.JPG',
                 titolo: "Germania",
                 paragrafo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
             },
             {
-                url: 'img/05.JPG',
+                url: 'imgBonus/05.JPG',
                 titolo: "Paradise",
                 paragrafo: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
             },
         ],
-        indiceActiveCorrente: 0
+        currentImg: 0,
     },
     methods: {
-        imgPrecedente() {
-            let indiceNuovo = this.indiceActiveCorrente - 1;
-            if (indiceNuovo < 0) {
-                indiceNuovo = this.imgList.length - 1;
+        upButton() {
+            let nuovoIndice = this.currentImg - 1;
+            if (nuovoIndice < 0) {
+                nuovoIndice = this.imgList.length - 1;
             }
-            this.indiceActiveCorrente = indiceNuovo;
+            this.currentImg = nuovoIndice;
         },
-        imgSuccessiva() {
-            let nuovoIndice = this.indiceActiveCorrente + 1;
+        downButton() {
+            let nuovoIndice = this.currentImg + 1;
             if (nuovoIndice > this.imgList.length - 1) {
                 nuovoIndice = 0;
             }
-            this.indiceActiveCorrente = nuovoIndice;
+            this.currentImg = nuovoIndice;
         }
+    },
+    mounted() {
+        setInterval(() => {
+            this.downButton();
+        }, 1500);
     }
 });
